b48c2383ab5886a88e26828356972324bba6db00
Eric D. White <eric@ericwhite.ca>
2009-06-14 13:35:07 +0200

    The start of some sample data for the site.

---
+++ public/javascripts/example-data.js
@@ -0,0 +1,200 @@
+/*
+ * Sample Album with images
+ */
+var sample_photos = {
+  "name": "2007/cote-dazur",
+  "tags": [
+    "Cote D'Azur", "summer", "France"
+    ],
+  "images": [
+    {
+      "id": "054901a0-3a56-012c-e7c7-00112436d9cc",
+      "name": "IMG_7275",
+      "org": "2007/cote-dazur/IMG_7275.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7275.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7275.JPG"
+    },
+    {
+      "id": "05492e70-3a56-012c-e7c8-00112436d9cc",
+      "name": "IMG_7276",
+      "org": "2007/cote-dazur/IMG_7276.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7276.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7276.JPG"
+    },
+    {
+      "id": "054987d0-3a56-012c-e7c9-00112436d9cc",
+      "name": "IMG_7277",
+      "org": "2007/cote-dazur/IMG_7277.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7277.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7277.JPG"
+    },
+    {
+      "id": "0549b8b0-3a56-012c-e7ca-00112436d9cc",
+      "name": "IMG_7278",
+      "org": "2007/cote-dazur/IMG_7278.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7278.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7278.JPG"
+    },
+    {
+      "id": "0549eaa0-3a56-012c-e7cb-00112436d9cc",
+      "name": "IMG_7279",
+      "org": "2007/cote-dazur/IMG_7279.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7279.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7279.JPG"
+    },
+    {
+      "id": "0552ae10-3a56-012c-e7cc-00112436d9cc",
+      "name": "IMG_7280",
+      "org": "2007/cote-dazur/IMG_7280.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7280.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7280.JPG"
+    },
+    {
+      "id": "0552dc10-3a56-012c-e7cd-00112436d9cc",
+      "name": "IMG_7281",
+      "org": "2007/cote-dazur/IMG_7281.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7281.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7281.JPG"
+    },
+    {
+      "id": "055309d0-3a56-012c-e7ce-00112436d9cc",
+      "name": "IMG_7282",
+      "org": "2007/cote-dazur/IMG_7282.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7282.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7282.JPG"
+    },
+    {
+      "id": "05532e80-3a56-012c-e7cf-00112436d9cc",
+      "name": "IMG_7283",
+      "org": "2007/cote-dazur/IMG_7283.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7283.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7283.JPG"
+    },
+    {
+      "id": "05535240-3a56-012c-e7d0-00112436d9cc",
+      "name": "IMG_7285",
+      "org": "2007/cote-dazur/IMG_7285.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7285.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7285.JPG"
+    },
+    {
+      "id": "05652180-3a56-012c-e7d1-00112436d9cc",
+      "name": "IMG_7286",
+      "org": "2007/cote-dazur/IMG_7286.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7286.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7286.JPG"
+    },
+    {
+      "id": "05654d90-3a56-012c-e7d2-00112436d9cc",
+      "name": "IMG_7287",
+      "org": "2007/cote-dazur/IMG_7287.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7287.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7287.JPG"
+    },
+    {
+      "id": "056572f0-3a56-012c-e7d3-00112436d9cc",
+      "name": "IMG_7342",
+      "org": "2007/cote-dazur/IMG_7342.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7342.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7342.JPG"
+    },
+    {
+      "id": "05659470-3a56-012c-e7d4-00112436d9cc",
+      "name": "IMG_7343",
+      "org": "2007/cote-dazur/IMG_7343.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7343.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7343.JPG"
+    },
+    {
+      "id": "0565bad0-3a56-012c-e7d5-00112436d9cc",
+      "name": "IMG_7344",
+      "org": "2007/cote-dazur/IMG_7344.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7344.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7344.JPG"
+    },
+    {
+      "id": "0577aba0-3a56-012c-e7d6-00112436d9cc",
+      "name": "IMG_7345",
+      "org": "2007/cote-dazur/IMG_7345.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7345.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7345.JPG"
+    },
+    {
+      "id": "05781ee0-3a56-012c-e7d7-00112436d9cc",
+      "name": "IMG_7346",
+      "org": "2007/cote-dazur/IMG_7346.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7346.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7346.JPG"
+    },
+    {
+      "id": "057853c0-3a56-012c-e7d8-00112436d9cc",
+      "name": "IMG_7347",
+      "org": "2007/cote-dazur/IMG_7347.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7347.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7347.JPG"
+    },
+    {
+      "id": "05787c80-3a56-012c-e7d9-00112436d9cc",
+      "name": "IMG_7348",
+      "org": "2007/cote-dazur/IMG_7348.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7348.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7348.JPG"
+    },
+    {
+      "id": "0578a160-3a56-012c-e7da-00112436d9cc",
+      "name": "IMG_7349",
+      "org": "2007/cote-dazur/IMG_7349.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7349.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7349.JPG"
+    },
+    {
+      "id": "058a5920-3a56-012c-e7db-00112436d9cc",
+      "name": "IMG_7350",
+      "org": "2007/cote-dazur/IMG_7350.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7350.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7350.JPG"
+    },
+    {
+      "id": "058a8510-3a56-012c-e7dc-00112436d9cc",
+      "name": "IMG_7351",
+      "org": "2007/cote-dazur/IMG_7351.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7351.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7351.JPG"
+    },
+    {
+      "id": "058ab170-3a56-012c-e7dd-00112436d9cc",
+      "name": "IMG_7352",
+      "org": "2007/cote-dazur/IMG_7352.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7352.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7352.JPG"
+    },
+    {
+      "id": "058ad650-3a56-012c-e7de-00112436d9cc",
+      "name": "IMG_7353",
+      "org": "2007/cote-dazur/IMG_7353.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7353.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7353.JPG"
+    },
+    {
+      "id": "059cd6c0-3a56-012c-e7df-00112436d9cc",
+      "name": "IMG_7354",
+      "org": "2007/cote-dazur/IMG_7354.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7354.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7354.JPG"
+    },
+    {
+      "id": "059d09d0-3a56-012c-e7e0-00112436d9cc",
+      "name": "IMG_7355",
+      "org": "2007/cote-dazur/IMG_7355.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7355.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7355.JPG"
+    },
+    {
+      "id": "059d3b50-3a56-012c-e7e1-00112436d9cc",
+      "name": "IMG_7356",
+      "org": "2007/cote-dazur/IMG_7356.JPG",
+      "slide": "2007/cote-dazur/slides/IMG_7356.JPG",
+      "thumb": "2007/cote-dazur/thumbs/IMG_7356.JPG"
+    }
+  ]
+};
